64552362f214db12497116825f48061418a0891e
Thibault MORELLE <thibault.morelle@gmail.com>
2019-05-15 11:25:06 +0200

    Update Name

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Installs IntelliJ templates
+
+echo "Installing template files..."
+
+find /Applications -path "*.app" -prune \( -name "*Android Studio*" -or -name "*IntelliJ IDEA*" \) -print0 | while read -d $'\0' folder
+do
+  echo "\nInstalling to $folder"
+  cp -frv $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/AMGP* "$folder/Contents/plugins/android/lib/templates/AMGP/"
+done
+
+echo "Done."
+echo ""
+echo "Restart IntelliJ and/or AndroidStudio"
