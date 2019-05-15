1810c14ac2910126a5afb0375eb28b15c22c4a78
Thibault MORELLE <thibault.morelle@gmail.com>
2019-05-15 13:15:57 +0200

    Fix Install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# Installs IntelliJ templates
-
 echo "Installing template files..."
 
 find /Applications -path "*.app" -prune \( -name "*Android Studio*" -or -name "*IntelliJ IDEA*" \) -print0 | while read -d $'\0' folder
 do
-  echo "\nInstalling to $folder"
-  cp -frv $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/AMGP* "$folder/Contents/plugins/android/lib/templates/AMGP/"
+  echo "Installing to $folder"
+  rm -rf "$folder/Contents/plugins/android/lib/templates/other/TMoreMVP" || true
+  mkdir -p "$folder/Contents/plugins/android/lib/templates/other/TMoreMVP" || true
+  cp -R $(pwd)/* "$folder/Contents/plugins/android/lib/templates/other/TMoreMVP"
 done
 
-echo "Done."
-echo ""
 echo "Restart IntelliJ and/or AndroidStudio"
